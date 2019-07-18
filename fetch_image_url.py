0504fb78e2a14f621c91560ede9c8cbcdafc3543
thiswind <thiswind@gmail.com>
2019-07-18 08:50:52 +0800

    fix: downgrade to meet python 3.5

--- fetch_image_url.py
+++ fetch_image_url.py
@@ -2,16 +2,15 @@ import sys
 line = sys.stdin.buffer.read()
 
 from bs4 import BeautifulSoup
-from typing import List
 import sys
 
 
 bs = BeautifulSoup(line, features='lxml')
-link_list:List = bs.findAll('a')
+link_list = bs.findAll('a')
 if len(link_list) == 1:
     link = link_list[0]
-    image_file_name:str = link.get('href')
-    print(f'https://static.redox-os.org/img/{image_file_name}'.strip())
+    image_file_name= link.get('href')
+    print('https://static.redox-os.org/img/{}'.format(image_file_name).strip())
 else:
     sys.exit(1)
 
